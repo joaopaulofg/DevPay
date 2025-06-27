c459a47d133a32637dd5e22d8e8987d9ea12ece5
joaopaulofg <joaofpaulo1@gmail.com>
2025-06-27 13:16:30 -0300

    atualizando dependencias e Usuario

--- back-end/cadastro-app/src/main/java/com/joaopaulofg/cadastroapp/entity/Usuario.java
+++ back-end/cadastro-app/src/main/java/com/joaopaulofg/cadastroapp/entity/Usuario.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -29,4 +34,38 @@ public class Usuario implements UserDetails {
 
     private Double renda;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return telefone;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
